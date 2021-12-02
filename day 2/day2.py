b88a9765f793c6a1ac58fb6b07c30cfde205c1d7
Jet Simon <jetjdsimon@gmail.com>
2021-12-01 21:11:26 -0800

    day 2 - had a typo, i swear i could have broken top 1000

--- day 2/day2.py
+++ day 2/day2.py
@@ -8,8 +8,38 @@ def loadData():
 
 data = loadData()
 
-def solve_1():
-    pass
+def solve_1(data):
+    pos = 0
+    depth = 0
+    for inst in data:
+        raw = inst.split(" ")
+        direction = raw[0]
+        amt=  int(raw[1])
+        if(direction == "forward"):
+            pos += amt
+        elif(direction == "down"):
+            depth += amt
+        elif(direction == "up"):
+            depth -= amt
+    return pos * depth
 
-def solve_2():
-    pass
+def solve_2(data):
+    pos = 0
+    depth = 0
+    aim = 0
+    for inst in data:
+        raw = inst.split(" ")
+        direction = raw[0]
+        amt=  int(raw[1])
+        if(direction == "forward"):
+            pos += amt
+            depth += amt * aim
+        elif(direction == "down"):
+            aim += amt
+        elif(direction == "up"):
+            aim -= amt
+    return pos * depth
+
+print("Part 1:", solve_1(data))
+
+print("Part 2:", solve_2(data))
