a185b64f17ed1ce98f504e37dabfb040d6ef53f4
Jet Simon <jetjdsimon@gmail.com>
2021-12-05 22:06:32 -0800

    day 6 - barked up the wrong tree for a sec

--- day 6/day6.py
+++ day 6/day6.py
@@ -1,17 +1,69 @@
 import itertools, re
+from math import e, log
 
 def loadData():
+    out = None
     f = open('input.txt')
-    out = [line.strip() for line in f]
+    for line in f:
+        out = line.split(",")
+        out = [int(n) for n in out]
     f.close()
     return out
 
+class Fish():
+    def __init__(self, pool, timer=8):
+        self.timer = timer
+        self.pool = pool
+    def cycle(self):
+        self.timer -= 1
+        if(self.timer < 0):
+            self.timer = 6
+            return 1
+        return 0
 
 def solve_1():
     data = loadData()
+    pool = []
+    pool = [Fish(pool,timer=n) for n in data]
+    toAdd = 0
+    for day in range(18):
+        #if(day % 1 == 0):
+            #print("day",day,":",len(pool))
+        for fish in pool:
+            #print(fish.timer)
+            toAdd += fish.cycle()
+        for i in range(toAdd):
+            pool.append(Fish(pool))
+        toAdd = 0
+    return len(pool)
 
-def solve_2():  
-    data = loadData()
+def solve_2():
+    maxDays = 256
+    data = {}  
+    rawData = loadData()
+    total = len(rawData)
+    for p in set(rawData):
+        data[p] = rawData.count(p)
+
+    for day in range(maxDays):
+        #print(total)
+        newData = {}
+        for fish in data:
+            newData[fish-1] = data[fish]
+        if 0 in data:
+            total += data[0]
+            if 8 in newData:
+                newData[8] += data[0]
+            else:
+                newData[8] = data[0]
+            if 6 in newData:
+                newData[6] += data[0]
+            else:
+                newData[6] = data[0]
+        data = newData
+
+
+    return total
 
 print("Part 1:", solve_1())
 
