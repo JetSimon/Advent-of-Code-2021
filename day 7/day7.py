7edf7a3bdb93f22c8b823a4e9902ddcce5cb0447
Jet Simon <jetjdsimon@gmail.com>
2021-12-06 21:12:23 -0800

    Day 7 - ezpz summezy

--- day 7/day7.py
+++ day 7/day7.py
@@ -2,14 +2,46 @@ import itertools, re
 
 def loadData():
     f = open('input.txt')
-    out = [line.strip() for line in f]
+    out = []
+    for line in f:
+        out = line.split(",")
+        out = [int(n) for n in out]
     f.close()
     return out
 
-data = loadData()
-
 def solve_1():
-    pass
+    data = loadData()
+    best = None
+    bestCost = sum(data)
+    for n in range(0,max(data)):
+        totalCost = 0
+        for e in data:
+            cost = abs(e-n)
+            totalCost += cost
+        if totalCost < bestCost:
+            bestCost = totalCost
+            best = n
+    return (best, "costs", bestCost)
+
+def sumUp(n):
+    return n*(n+1)/2
 
 def solve_2():
-    pass
+    data = loadData()
+    data = loadData()
+    best = None
+    bestCost = sum(data) * 10000
+    for n in range(0,max(data)):
+        totalCost = 0
+        for e in data:
+            cost = sumUp(abs(e-n))
+            totalCost += cost
+        if totalCost < bestCost:
+            bestCost = totalCost
+            best = n
+    return (best, "costs", bestCost)
+
+
+print("Part 1:", solve_1())
+
+print("Part 2:", solve_2())
